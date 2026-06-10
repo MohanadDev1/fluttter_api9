5a12b171efd59876d50ed3d0d9fe07dbb1ea62c5
Mohanad Hani <moha12nadd@gmail.com>
2026-06-11 00:02:08 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
-// نقطة البداية للمشروع
 void main() {
   runApp(MyApp());
 }
 
-// هذا الكلاس الأساسي للتطبيق
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // إخفاء علامة الـ debug من الزاوية
+      debugShowCheckedModeBanner: false,  debug من الزاوية
       title: 'Fetch Products API',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // تحديد الشاشة الأولى التي ستظهر للمستخدم
       home: HomeScreen(),
     );
   }
